9faf74b6313fc516a836e95d45b3b4abd155d0b7
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2022-08-01 09:53:50 +0900

    packages/commerce/scr/cnfig.cjs

--- packages/commerce/src/config.cjs
+++ packages/commerce/src/config.cjs
@@ -9,23 +9,26 @@ const importCwd = require('import-cwd')
 function withCommerceConfig(nextConfig = {}) {
   const commerce = nextConfig.commerce || {}
   const { provider } = commerce
+  const newProvider = provider.split('-')[1]
 
-  if (!provider) {
+  if (!newProvider) {
     throw new Error(
       `The commerce provider is missing, please add a valid provider name`
     )
   }
 
-  const commerceNextConfig = importCwd(path.posix.join(provider, 'next.config'))
-  const config = merge(nextConfig, commerceNextConfig)
-  const features = merge(
-    config.commerce.features,
-    config.commerce[provider]?.features ?? {}
+  process.chdir('../')
+  process.chdir('./packages')
+  const commerceNextConfig = importCwd(
+    path.join(process.cwd(), newProvider, 'src', 'next.config.cjs')
   )
+  const config = merge(nextConfig, commerceNextConfig)
+  process.chdir('../')
+  process.chdir('./site')
 
   config.env = config.env || {}
 
-  Object.entries(features).forEach(([k, v]) => {
+  Object.entries(config.commerce.features).forEach(([k, v]) => {
     if (v) config.env[`COMMERCE_${k.toUpperCase()}_ENABLED`] = true
   })
 
@@ -33,3 +36,35 @@ function withCommerceConfig(nextConfig = {}) {
 }
 
 module.exports = { withCommerceConfig }
+
+// const path = require('path')
+// const merge = require('deepmerge')
+// const importCwd = require('import-cwd')
+
+// function withCommerceConfig(nextConfig = {}) {
+//   const commerce = nextConfig.commerce || {}
+//   const { provider } = commerce
+
+//   if (!provider) {
+//     throw new Error(
+//       `The commerce provider is missing, please add a valid provider name`
+//     )
+//   }
+
+//   const commerceNextConfig = importCwd(path.posix.join(provider, 'next.config'))
+//   const config = merge(nextConfig, commerceNextConfig)
+//   const features = merge(
+//     config.commerce.features,
+//     config.commerce[provider]?.features ?? {}
+//   )
+
+//   config.env = config.env || {}
+
+//   Object.entries(features).forEach(([k, v]) => {
+//     if (v) config.env[`COMMERCE_${k.toUpperCase()}_ENABLED`] = true
+//   })
+
+//   return config
+// }
+
+// module.exports = { withCommerceConfig }
